35c5f670af51ee67451fe99c9eef81ae416de613
Gosia <gosia@acer>
2016-02-24 22:15:43 +0100

    # consoleApplications - usos

---
+++ src/main/java/pl/lodz/usos/ConsoleApplications.java
@@ -0,0 +1,38 @@
+package pl.lodz.usos;
+
+import java.util.Scanner;
+
+/**
+ * Created by stawicad on 2016-02-10.
+ */
+public class ConsoleApplications {
+	
+	public static void main(String[] args) {
+		
+	StudentService ss = new StudentService();
+	
+
+	while(true){
+		System.out.println("");
+		System.out.println("1 - add the students");
+		System.out.println("2 - show all students");
+		System.out.println("3 - wyszukaj studenta po indeksie");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("give the number");
+		String a =sc.nextLine();
+		if("1".equals(a)){
+		System.out.println("give the name");
+		String name = sc.nextLine();
+		System.out.println("give the surname");
+		String surname = sc.nextLine();
+		System.out.println("give the index");
+		String index = sc.nextLine();
+		
+		ss.addNewStudent(new Student (index , name ,surname));
+		}
+		
+	}
+	}
+	
+}
