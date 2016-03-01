b1b0875d732b52c1f7048c30301f5505c0f1c836
Gosia <gosia@acer>
2016-03-01 23:54:02 +0100

    #3

--- src/main/java/pl/lodz/usos/FileDao.java
+++ src/main/java/pl/lodz/usos/FileDao.java
@@ -1,21 +1,40 @@
 package pl.lodz.usos;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Scanner;
 
-public class FileDao {
-	
-	public FileDao( String plik){
-		
-		File f = new File(plik);
+public class FileDao implements StudentDao {
+
+	private File file;
+
+	public FileDao(String plik) {
+
+		file = new File(plik);
 	}
-	
-public List<Student> getAllStudents() {
-    	
-        return null;
-    }
-public boolean addNewStudent(Student s) {
-	
-    return true;
-}
+
+	public List<Student> getAllStudents() throws FileNotFoundException {
+		Scanner in = new Scanner(file);
+		while(in.hasNextLine()){
+			String read = in.nextLine();
+			read.split(";");
+			getAllStudents().add(new Student(read, read,read));
+			
+			
+		}
+		return null;
+	}
+
+	public boolean addStudent(Student s) throws FileNotFoundException {
+		new PrintWriter(file);
+		
+		return true;
+	}
+
+	public Student getStudent(String index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/main/java/pl/lodz/usos/StudentService.java
+++ src/main/java/pl/lodz/usos/StudentService.java
@@ -1,5 +1,6 @@
 package pl.lodz.usos;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,22 +9,24 @@ import java.util.List;
  */
 public class StudentService {
 	
-	private List<Student> students = new ArrayList<Student>();
+	//private List<Student> students = new ArrayList<Student>();
+	private FileDao dao = new FileDao("baza.txt");
 
-    public boolean addNewStudent(Student s) {
-    	students.add(s);
+    public boolean addNewStudent(Student s) throws FileNotFoundException {
+    	dao.addStudent(s);
         return true;
     }
 
-    public List<Student> getAllStudents() {
+    public List<Student> getAllStudents() throws FileNotFoundException {
     	
-        return students;
+        return dao.getAllStudents();
     }
 
-    public Student getStudent(String index) {
-    	for (int i = 0; i < students.size(); i++) {
-			if(students.get(i).index.equals(index)){
-				return students.get(i);
+    public Student getStudent(String index) throws FileNotFoundException {
+    	List<Student> allStudents = dao.getAllStudents();
+		for (int i = 0; i < allStudents.size(); i++) {
+			if(allStudents.get(i).index.equals(index)){
+				return allStudents.get(i);
 			}
 		}
         return null;
