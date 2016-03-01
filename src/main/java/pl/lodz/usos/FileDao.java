39ce55f336b99885d2f7f1f0fd60a67525f9f8d5
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2016-03-01 15:45:36 +0100

    #3

--- src/main/java/pl/lodz/usos/FileDao.java
+++ src/main/java/pl/lodz/usos/FileDao.java
@@ -1,11 +1,21 @@
 package pl.lodz.usos;
 
 import java.io.File;
+import java.util.List;
 
 public class FileDao {
 	
-	public FileDao(){
-		File f = new File(".");
+	public FileDao( String plik){
+		
+		File f = new File(plik);
 	}
-
+	
+public List<Student> getAllStudents() {
+    	
+        return null;
+    }
+public boolean addNewStudent(Student s) {
+	
+    return true;
+}
 }
