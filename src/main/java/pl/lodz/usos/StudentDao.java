238c6bdf36cccb23cf738fb72a78d62119b0c519
Gosia <gosia@acer>
2016-03-02 14:35:22 +0100

    #3 studentDao

---
+++ src/main/java/pl/lodz/usos/StudentDao.java
@@ -0,0 +1,13 @@
+package pl.lodz.usos;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface StudentDao {
+	public List<Student> getAllStudents() throws FileNotFoundException;
+
+	public boolean addStudent(Student s) throws FileNotFoundException;
+
+	public Student getStudent(String index);
+
+}
